3e71372d008ea7b4cecb527b450af9ae8284abe5
Sweta Singh <120044039+swetasingh8844@users.noreply.github.com>
2024-01-16 18:40:53 +0530

    Add a dark theme functionality

--- frontend/forum/src/App.jsx
+++ frontend/forum/src/App.jsx
@@ -1,7 +1,45 @@
-import AllRoutes from "./shared/routes/AllRoutes";
+
+
+import React, { useState } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AllRoutes from './shared/routes/AllRoutes';
+import Button from '@mui/material/Button';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const App = () => {
-  return <AllRoutes />;
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: "'Roboto Mono', monospace;",
+    },
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Button
+        style={{ position: 'fixed', top: 10, right: 10 }}
+        onClick={toggleDarkMode}
+        color="inherit"
+      >
+        {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+      </Button>
+      <AllRoutes />
+    </ThemeProvider>
+  );
 };
 
 export default App;
+
+
+
