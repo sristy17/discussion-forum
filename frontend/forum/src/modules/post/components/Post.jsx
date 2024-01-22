ad12821351d302c94732fe817ebc65961875ff69
Shabin-k <helloshabink@gmail.com>
2024-01-22 23:44:16 +0530

    refactor : improve readabilty

--- frontend/forum/src/modules/post/components/Post.jsx
+++ frontend/forum/src/modules/post/components/Post.jsx
@@ -46,7 +46,8 @@ const Post = ({ post }) => {
       </div>
       <div>
         <Link
-          to={`/discuss/${post.slug}`} state={{description: post.text}} 
+          to={`/discuss/${post.slug}`}
+          state={{ description: post.text }}
           style={{ textDecoration: "none", color: "black" }}
         >
           <Typography variant="h5" component="div">
